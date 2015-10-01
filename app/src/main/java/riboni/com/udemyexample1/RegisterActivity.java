1e34e0ef32c6b4fb98909670e05f9f8a67c221dd
Riboni1989 <riboni.1989@gmail.com>
2015-10-02 00:00:48 +0200

    Added Parse initialize

--- app/src/main/java/riboni/com/udemyexample1/RegisterActivity.java
+++ app/src/main/java/riboni/com/udemyexample1/RegisterActivity.java
@@ -2,10 +2,12 @@ package riboni.com.udemyexample1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
@@ -22,6 +24,8 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Parse.initialize(this, "yDtXoC5vV5UQ29DUXMkzb66WeI3QltDQZLMxCMUC", "oApQ0fw7aVUoMBL0I5mVZumrHBXfTfATtiDO5q63");
+
         // initialize
         mUsername = (EditText) findViewById(R.id.registerUsernameEditText);
         mUserEmail = (EditText) findViewById(R.id.registerEmailEditText);
@@ -31,7 +35,7 @@ public class RegisterActivity extends Activity {
     }
 
     /** Called when the user clicks the SignUp button */
-    public void signUp(){
+    public void signUp(View view){
         ParseUser user = new ParseUser();
         user.setUsername(mUsername.getText().toString().trim());
         user.setEmail(mUserEmail.getText().toString().trim());
