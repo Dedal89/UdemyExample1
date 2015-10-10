63550512c84d5a960ac44060a84e2a984ba5787f
Riboni1989 <riboni.1989@gmail.com>
2015-10-10 22:05:34 +0200

    added menu with update status or logout options

--- app/src/main/java/riboni/com/udemyexample1/HomeActivity.java
+++ app/src/main/java/riboni/com/udemyexample1/HomeActivity.java
@@ -1,11 +1,13 @@
 package riboni.com.udemyexample1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,17 +15,40 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser == null) {
+            Intent takeLogin = new Intent(this, LoginActivity.class);
+            startActivity(takeLogin);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.updateStatus:
+                Intent takeUpdateStatus = new Intent(this, UpdateStatusActivity.class);
+                startActivity(takeUpdateStatus);
+                break;
+            case R.id.logoutUser:
+                ParseUser.logOut();
+                Intent takeLogin = new Intent(this, LoginActivity.class);
+                startActivity(takeLogin);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
